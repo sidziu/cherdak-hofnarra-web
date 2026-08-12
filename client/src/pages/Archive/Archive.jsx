7979b777144874d86f5e1c44d2f32580e78e7153
sidziu <sidziu16may@gmail.com>
2026-08-12 17:04:10 +0300

    Допил дизайна и revie кода страницы Архива спектаклей

--- client/src/pages/Archive/Archive.jsx
+++ client/src/pages/Archive/Archive.jsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./Archive.css";
 
+import errorImage from "../../assets/logotypes/logo-meow-red-withbg.png";
+
 import { API } from '../../api'; // Импортируем обьект API с функциями для запросов на сервер
 
 function Archive() {
@@ -38,45 +40,41 @@ function Archive() {
 
     return (
         <div className="archive-container">
-            <h1 className="archive-title">Архив спектаклей</h1>
+            <h1 className="archive-title">Прошедшие спектакли</h1>
 
             {loading && <p className="archive-status">Загрузка архивных материалов...</p>}
             {error && <p className="archive-error">{error}</p>}
 
             <div className="archive-grid">
-                {performances.map((perf) => {
-                    const imageFilename = perf.image || (perf.imageUrl ? perf.imageUrl.split("/").pop() : "");
-
-                    return (
-                        <div key={perf.id} className="archive-card">
-                            
-                            <div className="archive-img-container">
-                                <img 
-                                    src={perf.imageUrl} 
-                                    alt={perf.title} 
-                                    className="archive-card-img"
-                                    onError={(e) => {
-                                        e.target.src = "https://images.unsplash.com/photo-1516307365426-bea591f05011?q=80&w=400";
+                {performances.map((perf) => { return (
+                    <Link  to={`/archive/${perf.id}`}  key={perf.id} className="archive-card">
+                         <div className="archive-img-container">
+                            <img 
+                                src={perf.imageUrl} 
+                                alt={perf.title} 
+                                className="archive-card-img"
+                                onError={(e) => {
+                                        e.target.src = errorImage;
                                     }}
-                                />
-                            </div>
-
-                            <div className="archive-card-info">
-                                <span className="archive-card-genre">{perf.genre}</span>
-                                <h2 className="archive-card-title">{perf.title}</h2>
-                                <p className="archive-card-director">Режиссёр: {perf.director}</p>
-                                
-                                <Link 
-                                    to={`/archive/${perf.id}`} 
-                                    state={{ title: perf.title }}
-                                    className="archive-more-btn"
-                                >
-                                    Подробнее: запись, фото
-                                </Link>
-                            </div>
-
+                            />
                         </div>
-                    );
+                        
+                        <div className="archive-card-info">
+                            <span className="archive-card-genre">{perf.genre}</span>
+                            <h2 className="archive-card-title">{perf.title}</h2>
+                            <p className="archive-card-director">Режиссёр: {perf.director}</p>
+                                
+                            <Link 
+                                to={`/archive/${perf.id}`} 
+                                state={{ title: perf.title }}
+                                className="archive-detail-btn"
+                            >
+                                Подробнее: запись, фото
+                            </Link>
+                        </div>
+
+                    </Link>
+                );
                 })}
             </div>
         </div>
